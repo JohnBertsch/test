e6c12a556ee1b7f9a5fd20a5a720053d12d7cfcc
Christian <c.esteves92@gmail.com>
2020-11-19 01:22:47 +0000

    Created the Node class

---
+++ linkedlist.cpp
@@ -0,0 +1,3 @@
+LinkedList::LinkedList(){
+    this->root = nullptr;
+}
---
+++ linkedlist.h
@@ -0,0 +1,14 @@
+class Node{
+    public:
+        int data;
+        Node* next;
+}
+
+
+class LinkedList{
+    private:
+        Node* root;
+
+    public:
+        LinkedList();
+}
